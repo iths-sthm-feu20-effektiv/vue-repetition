8de7aa2ecfce1fd7d62d3081db07a6e7bf7cd8df
David Andersson <lejonmanen@gmail.com>
2021-04-08 10:49:28 +0200

    Prototype with static data

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 # vue-repetition
 
-## Project setup
-```
-npm install
-```
+This web app was made during the study week. Repetition of Vue concepts.
+
+It shows my favorite movies. Users can add movies and rate them.
+
+## What we did to create this app
+
+1. Brainstorming, Figma wireframe
+2. Prototype in Figma
+3. Create Vue project, create GitHub repository
+4. Implement prototype in Vue (one component, only HTML/template)
+5. Create more components
+6. Make them intelligent
+
+
 
 ### Compiles and hot-reloads for development
 ```
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Link to Figma prototype:
+https://www.figma.com/file/HMC5bd7xpBJuWYdq3c1yXs/Vue-repetition-studerandevecka?node-id=3%3A47
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <header>
+            Mina favoritfilmer
+        </header>
+    </div>
+    <main>
+        <div class="movie-list">
+            <div class="movie-item">
+                <div>
+                    Pulp fiction
+                </div>
+                <div>
+                    Betyg: 5 <br/>
+                    2021-02-21
+                </div>
+            </div>
+        </div>
+        <div class="movie-list">
+            <div class="movie-item">
+                <div>
+                    Meet the feebles
+                </div>
+                <div>
+                    Betyg: 4 <br/>
+                    2020-10-11
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: { }
 }
 </script>
 
 <style>
+header {
+    background-color: #DB2818;
+    color: #EBAD24;
+    padding: 1.5em;
+    margin: 0em;
+}
+.movie-item {
+    max-width: 40em;
+    border: 1px solid #DB2818;
+    padding: 1em;
+    margin: 0.3em;
+    display: flex;
+    /* vh, vw utgår från viewport - webbläsarens synliga område */
+}
+.movie-item > * {
+    flex: 1 1 auto;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin: 0px;
+}
+body {
+    margin: 0em;
 }
 </style>
